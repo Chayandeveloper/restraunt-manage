aec99231ab3634cc41604be6cfa1bee88f24fc75
Chayan Biswas <biswaschayan613@gmail.com>
2026-05-12 23:26:46 +0530

    Describe your changes here

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,3 +5,11 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<React.StrictMode><App /></React.StrictMode>);
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(reg => console.log('SW registered:', reg))
+      .catch(err => console.log('SW registration failed:', err));
+  });
+}
